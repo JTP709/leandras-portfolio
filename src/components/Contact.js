8a7cf6fbfedbfbe093b737acb78809c5671a916a
Jon <JTP709@gmail.com>
2019-06-29 19:23:12 -0400

    add recaptcha

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,16 +8,27 @@ const Contact = () => (
         Are you as ready as I am to get started? Contact me using the form below!<br />
         Or please feel free to contact me on social media.
       </p>
-      <form name="contact" method="POST" data-netlify="true" action="/success">
-          <label>Name:
-            <input type="text" name="name" />
-          </label>
-          <label>E-Mail:
-            <input type="email" name="email" />
-          </label>
-          <label>Message:
-            <textarea name="message" />
-          </label>
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        data-netlify-recaptcha="true"
+        netlify-honeypot="bot-field"
+        action="/success"
+      >
+        <label class="bot-field">Title:
+          <input name="bot-field"  type="text" />
+        </label>
+        <label>Name:
+          <input type="text" name="name" />
+        </label>
+        <label>E-Mail:
+          <input type="email" name="email" />
+        </label>
+        <label>Message:
+          <textarea name="message" />
+        </label>
+        <div data-netlify-recaptcha="true"></div>
         <button className="button" type="submit">Submit</button>
       </form>
     </header>
